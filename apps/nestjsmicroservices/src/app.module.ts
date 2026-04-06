f07cde795f8a4c00aaa88d54f3da65df1aaade33
utibe solomon <utibesolomon6@gmail.com>
2026-04-06 16:58:21 +0100

    Added nestjs Pino For better Logging

--- apps/nestjsmicroservices/src/app.module.ts
+++ apps/nestjsmicroservices/src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsModule , Transport } from '@nestjs/microservices';
-import { join } from 'path';
 import { UsersModule } from './users/users.module';
 import { WalletModule } from './wallet/wallet.module';
-
+import { LoggerModule } from 'nestjs-pino';
 @Module({
   imports: [
+  LoggerModule.forRoot({
+      pinoHttp: {
+        transport: process.env.NODE_ENV !== 'production' 
+          ? { target: 'pino-pretty' } 
+          : undefined,
+      },
+    }),
     UsersModule,
     WalletModule
   ],
--- apps/nestjsmicroservices/src/main.ts
+++ apps/nestjsmicroservices/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { LoggingInterceptor } from '@app/packages';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useLogger(app.get(Logger))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
